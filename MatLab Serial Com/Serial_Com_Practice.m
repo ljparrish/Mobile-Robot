9c862e6905e0af3c2f99dc75c96041fde57de38e
williamx01 <williamx0713@gmail.com>
2024-04-04 22:56:32 -0700

    Updated GUI and serial comms

--- MatLab Serial Com/Serial_Com_Practice.m
+++ MatLab Serial Com/Serial_Com_Practice.m
@@ -3,6 +3,14 @@ close all
 clear all
 clc
 
+% create ESP32 Object
+espObj = arduino("COM4", "WALE Robot", "Libraries", {'I2C','Serial','SPI'});
 % create serial object
-serialdevObj = device(arduinoObj,'COM4',1,'BaudRate', 115200);
+serialdevObj = device(espObj,'COM4',1,'BaudRate', 115200);
+
+% write 3 bytes of data to the serial device
+write(serialdevObj, [88 99 45]);
+
+% read data from the serial device
+read(serialdevObj, 3)
 
