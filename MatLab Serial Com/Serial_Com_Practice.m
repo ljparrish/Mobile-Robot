44e9933a5eae85f33f1442df8a4abf70b37c2179
williamx01 <williamx0713@gmail.com>
2024-04-09 12:38:13 -0700

    Update Serial Comm

--- MatLab Serial Com/Serial_Com_Practice.m
+++ MatLab Serial Com/Serial_Com_Practice.m
@@ -3,14 +3,23 @@ close all
 clear all
 clc
 
-% create ESP32 Object
-espObj = arduino("COM4", "WALE Robot", "Libraries", {'I2C','Serial','SPI'});
-% create serial object
-serialdevObj = device(espObj,'COM4',1,'BaudRate', 115200);
+% % create ESP32 Object
+% espObj = arduino("COM3", "WALE Robot", "Libraries", {'Serial'});
+% % create serial object
+% serialdevObj = device(espObj,'COM3',1,'BaudRate', 115200);
+% 
+% % write 3 bytes of data to the serial device
+% write(serialdevObj, [88 99 45]);
+% 
+% % read data from the serial device
+% read(serialdevObj, 3)
 
-% write 3 bytes of data to the serial device
-write(serialdevObj, [88 99 45]);
+comChannel = serialportlist;
 
-% read data from the serial device
-read(serialdevObj, 3)
+s = serialport(comChannel(1),9600);
+data = [88, 99, 45];
+write(s, data, "int16")
 
+r = read(s,256,"int16");
+
+disp(r)
