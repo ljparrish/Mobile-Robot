bec64b057554bfa421336ba5751326ca13390b71
ljparrish <liamparrish1998@gmail.com>
2024-04-09 12:00:16 -0700

    Created USB dongle ESP-IDF Project
    Setup project folder for USB wireless dongle using ESP32-C3

---
+++ Embedded-SW/mobile-robot-dongle/main/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+#include "freertos/queue.h"
+
+// Include source files for Mobile Robot Dongle Here!
+
+// Setup any Queues here!
+
+// Write RTOS Callback functions here! (The RTOS task can also be defined in your .c file)
+
+void app_main(void)
+{
+
+}
