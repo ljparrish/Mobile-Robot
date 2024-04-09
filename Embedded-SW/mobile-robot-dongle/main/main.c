d4e7f2b3fccb688e3141323df39818950a8493e4
ljparrish <liamparrish1998@gmail.com>
2024-04-09 14:22:28 -0700

    Started UART.c File

--- Embedded-SW/mobile-robot-dongle/main/main.c
+++ Embedded-SW/mobile-robot-dongle/main/main.c
@@ -7,12 +7,14 @@
 #include "freertos/queue.h"
 
 // Include source files for Mobile Robot Dongle Here!
-
+#include "uart.c"
 // Setup any Queues here!
 
 // Write RTOS Callback functions here! (The RTOS task can also be defined in your .c file)
 
 void app_main(void)
 {
-
+    // Create RTOS Tasks here using xTaskCreate:
+    // Parameters: | Task callback function | Task Name | Memory Assigned to Task | Parameters to pass into the task | Priority | Task Handle
+    xTaskCreate(echo_task, "UART Echo", 2048, NULL, 1, NULL);
 }
