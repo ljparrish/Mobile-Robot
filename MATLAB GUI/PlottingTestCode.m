bed4089f2217897658d10d2459b20e9e2b703370
williamx01 <williamx0713@gmail.com>
2024-04-16 11:37:16 -0700

    Plotting Robot Function

--- MATLAB GUI/PlottingTestCode.m
+++ MATLAB GUI/PlottingTestCode.m
@@ -1,16 +1,16 @@
 clc; clear; close all;
 %% 
-xPos = [20, 20, 20, 20, 20, 20, 20, 20, 25];
-yPos = [10, 15, 20, 25, 30, 35, 40, 45, 55];
+xPos = 20;
+yPos = 10;
+theta = pi/4;
+theta = rad2deg(theta);
+
 robotWidth = 10; % Change values
 robotLength = 5; % Change values
 
 for i = 1:1:length(xPos)
 
-    X = xPos(i) - (robotWidth/2);
-    Y = yPos(i) - (robotLength/2);
-    pos = [X, Y, robotWidth, robotLength];
-    rectangle('Position', pos,'FaceColor',[0 0 0],'EdgeColor','r')
+    GenerateRobot(xPos(i), yPos(i), robotLength, robotWidth, 'r', theta)
 end
   
 
