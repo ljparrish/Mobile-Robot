42739b805334a8b1165ca26045149b15ef4405d2
ljparrish <125088225+ljparrish@users.noreply.github.com>
2024-04-16 08:16:47 -0700

    Dongle UART and Wireless
    - Added UART configuration for dongle
    - Updated ESP NOW structure to be smaller
    - Wrote task functions and setup queues for 2 way data transmission

--- Embedded-SW/mobile-robot-dongle/main/uart.c
+++ Embedded-SW/mobile-robot-dongle/main/uart.c
@@ -7,12 +7,12 @@
 #include "driver/uart.h"
 #include "string.h"
 
-#define ECHO_TEST_TXD (GPIO_NUM_6)
-#define ECHO_TEST_RXD (GPIO_NUM_7)
+#define ECHO_TEST_TXD (GPIO_NUM_10)
+#define ECHO_TEST_RXD (GPIO_NUM_9)
 #define ECHO_TEST_RTS (UART_PIN_NO_CHANGE)
 #define ECHO_TEST_CTS (UART_PIN_NO_CHANGE)
 
-#define ECHO_UART_PORT_NUM      (UART_NUM_0)
+#define ECHO_UART_PORT_NUM      (UART_NUM_1)
 #define ECHO_UART_BAUD_RATE     (115200)
 #define ECHO_TASK_STACK_SIZE    (2048)
 
@@ -85,4 +85,24 @@ void stream_task(void * arg)
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
     
+}
+
+void uart_setup(void)
+{
+    // Setup UART Parameters
+    uart_config_t uart_config = {
+        .baud_rate = ECHO_UART_BAUD_RATE,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    int intr_alloc_flags= 0;
+
+    // Install and Initialize the UART Drivers
+    ESP_ERROR_CHECK(uart_driver_install(ECHO_UART_PORT_NUM, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
+    ESP_ERROR_CHECK(uart_param_config(ECHO_UART_PORT_NUM, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(ECHO_UART_PORT_NUM, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS));
+
 }
