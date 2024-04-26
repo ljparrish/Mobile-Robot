78a128e17fda00f871c0ae840d682bd73b0cc8a9
williamx01 <williamx0713@gmail.com>
2024-04-25 23:46:54 -0700

    Update PlottingTestCode.m

--- Plotting Practice/PlottingTestCode.m
+++ Plotting Practice/PlottingTestCode.m
@@ -10,13 +10,9 @@ z = [2,4,6,8,10];
 y1 = [1,2,3,4,5];
 z1 = [2,4,6,8,10];
 hold on
-plot(x,y1,'-','Color',[0 0.4470 0.7410],'DisplayName','Actual \omega_{L} [rad/s]')
+plot(x, y,'.', 'MarkerSize',12,'Color',[0 0.4470 0.7410],'DisplayName','\omega_{L} [rad/s]')
 hold on
-plot(x,z1,'-','Color',[0.6350 0.0780 0.1840],'DisplayName','Actual \omega_{L} [rad/s]')
-hold on
-plot(x, y,'.', 'MarkerSize',12,'Color',[0 0.4470 0.7410],'DisplayName','Planned \omega_{L} [rad/s]')
-hold on
-plot(x,z,'.', 'MarkerSize',12,'Color',[0.6350 0.0780 0.1840],'DisplayName','Planned \omega_{L} [rad/s]')
+plot(x,z,'.', 'MarkerSize',12,'Color',[0.6350 0.0780 0.1840],'DisplayName','\omega_{L} [rad/s]')
 legend('Location','Southeast')
 
 hold off
